428698e3411e7efd61ce30e8939a887206b860e7
ivanduque0 <ivan6a69@gmail.com>
2022-09-28 14:30:14 -0400

    se completo la screen de ingreso de datos

--- appkivymd.py
+++ appkivymd.py
@@ -6,6 +6,7 @@ from kivymd.uix.label import MDLabel, MDIcon
 from kivymd.font_definitions import theme_font_styles
 from kivymd.uix.button import MDFloatingActionButton, MDFlatButton, MDRectangleFlatButton
 from kivymd.uix.screen import Screen
+from kivymd.uix.dialog import MDDialog
 import id_user_style
 from kivy.lang import Builder
 from kivymd.uix.dropdownitem import MDDropDownItem
@@ -14,8 +15,8 @@ URL='https://tesis-reconocimiento-facial.herokuapp.com/apertura/'
 CONTRATO="servidorarchpc"
 ID_USUARIO="1262315361"
 URL_CONTRATOS="https://tesis-reconocimiento-facial.herokuapp.com/agregarcontratosapi/"
-#store = DictStore('STOREXD')
-# store = JsonStore('jsonstore.json')
+#store = DictStore('datos_usuario')
+store = JsonStore('datos_usuario.json')
 # store.put('datos_usuario', contrato=CONTRATO, id_usuario=ID_USUARIO)
 
 
@@ -52,36 +53,61 @@ class seguricel_prototipo(MDApp):
         self.theme_cls.primary_palette = "Orange"
         self.theme_cls.primary_hue = "400"
         self.theme_cls.theme_style = "Dark"
-        
+        self.contrato_seleccionado = ''
+        self.cerrar_dialogo = MDFlatButton(text='cerrar',
+                                          on_release=self.cerrar_error)
+        self.dialogo = MDDialog(title='Error',
+                     size_hint=(0.7,0.3),
+                     buttons=[self.cerrar_dialogo])
         screen = Screen()
-        lista = Builder.load_string(id_user_style.lista_contratos)
-        screen.add_widget(lista)
-        boton_guardar = Builder.load_string(id_user_style.guardar_button)
-        boton_guardar.bind(on_press=self.guardar_usuario)
-        screen.add_widget(boton_guardar)
-        id_usuario = Builder.load_string(id_user_style.id_usuario_input)
-        screen.add_widget(id_usuario)
+        contratos_http = requests.get(url=URL_CONTRATOS).json()
+
         menu_items = [
             {
-                "text": f"Item {i}",
+                "text": f"{contrato['nombre']}",
                 "viewclass": "OneLineListItem",
-                "on_release": lambda x=f"Item {i}": self.menu_callback(x),
-            } for i in range(5)
+                "on_release": lambda x=f"{contrato['nombre']}": self.menu_callback(x),
+            } for contrato in contratos_http
         ]
         self.menu = MDDropdownMenu(
-            caller=screen,
             items=menu_items,
             width_mult=4,
         )
-        
+
+        self.lista = Builder.load_string(id_user_style.lista_contratos)
+        self.lista.bind(on_release=self.lista_contratos)
+        screen.add_widget(self.lista)
+        boton_guardar = Builder.load_string(id_user_style.guardar_button)
+        boton_guardar.bind(on_press=self.guardar_usuario)
+        screen.add_widget(boton_guardar)
+        self.id_usuario = Builder.load_string(id_user_style.id_usuario_input)
+        screen.add_widget(self.id_usuario)
         
         return screen
 
     def menu_callback(self, text_item):
-        print(text_item)
+        self.contrato_seleccionado = text_item
+        self.lista.set_item(text_item)
+        self.menu.dismiss()
 
     def guardar_usuario(self, instance):
-        print('accion')
+        if self.contrato_seleccionado and self.id_usuario.text:
+            store.put('datos_usuario', contrato=self.contrato_seleccionado, id_usuario=self.id_usuario.text)
+            print(self.id_usuario.text)
+            print(self.contrato_seleccionado)
+        elif self.contrato_seleccionado == '':
+            self.dialogo.text='Por favor seleccione un contrato'
+            self.dialogo.open()
+        elif self.id_usuario.text == '':
+            self.dialogo.text ='Por favor introduzca su ID'
+            self.dialogo.open()
+
+    def lista_contratos(self, caller):
+        self.menu.caller = caller
+        self.menu.open()
+
+    def cerrar_error(self, obj):
+        self.dialogo.dismiss()
 
     # def abrir_acceso(self, instance):
     #     requests.post(URL, 
--- id_user_style.py
+++ id_user_style.py
@@ -1,9 +1,9 @@
 
 id_usuario_input = """
 MDTextField:
-    hint_text: "Introduzca su ID"
+    hint_text: "Introducir ID"
     pos_hint:{'center_x': 0.5, 'center_y': 0.57}
-    size_hint_x:None
+    size_hint:(0.5, 0.1)
     width:200
 """
 
@@ -11,7 +11,7 @@ guardar_button = """
 MDFillRoundFlatButton:
     text:"Guardar datos"
     pos_hint:{'center_x': 0.5, 'center_y': 0.5}
-    size_hint_x:None
+    size_hint:(0.5,0.01)
     width:200
 """
 
@@ -21,8 +21,8 @@ MDDropDownItem:
     id: drop_item
     text: 'seleccione contrato'
     pos_hint: {'center_x': 0.5, 'center_y': 0.8}
-    dropdown_bg: [0, 1, 1, 1]  
-    on_release: app.menu.open()
+     
+
     
 """
 
